2b7b37f2f50af249d5b8881e1a2a6749bcc38eb0
RobertMaina <robertmaina254@gmail.com>
2021-09-19 14:54:05 +0300

    add user handle event

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+
+    onInputChange (e) {
+        console.log(e.target.value)
+    }
+
     render() {
         return (
             <div className= "ui segment">
                 <form className= "ui form">
                     <div className= "field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            onChange={this.onInputChange} //onChange: user changes text in an input
+                        />
                     </div>
                 </form>
             </div>
